b8d9c78da00234f499078a26bd592b5f78599a88
Maya Brandi <maya.brandi@tools.scilifelab.se>
2014-10-07 16:31:56 +0200

    Adding processes for nextera protocol

--- objectsDB.py
+++ objectsDB.py
@@ -97,7 +97,7 @@ class ProjectDB():
         KEY             lims_element    lims_field  descript
         ============    ============    =========== ================ 
         application     Project         Application -
-        samples         Sample          Name        hkjhkj hjkhkj j\
+        samples         Sample          Name        hkjhkj hjkhkj jhjkhjk\
                                                     hkjhkj hjkhkj j
         open_date       Project         open-date   -
         close_date      Project         close-date  -
