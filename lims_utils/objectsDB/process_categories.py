012e9600d80cea717e06c46d2e81180ea058b8c6
Maya Brandi <maya.brandi@tools.scilifelab.se>
2014-11-25 14:54:11 +0100

    dsd

--- lims_utils/objectsDB/process_categories.py
+++ lims_utils/objectsDB/process_categories.py
@@ -1,3 +1,5 @@
+GENERALINFO = """In the project-statusdb context, lims processes are categorised into groups that define, or are used to define a certain type of status-db key in a project database. The categories are specified here. When a new work flow is initialised in lims, the different categories needs to be updated to contain any aditional steps that has not already been included from some other workfrow. If a work flow does not fit with the categories one might have to change the category definitions or ad new categories. This needs to be done in corperation with the developer of project_summary_uppload_LIMS.py."""
+
 INITALQCFINISHEDLIB = {'Description':'All processes included in the initial qc protocol for finished libraries, except the aggregation step.',
     '24' : 'Customer Gel QC',
     '62' : 'qPCR QC (Library Validation) 4.0',
@@ -118,6 +120,7 @@ section_end = """=== =======================================
 
 def make_doc():
     f = open('process_categories.rst', 'w')
+    print >> f, GENERALINFO
     for cat in PROCESSCATEGORIES:
         print >> f, cat
         print >> f, section_start
