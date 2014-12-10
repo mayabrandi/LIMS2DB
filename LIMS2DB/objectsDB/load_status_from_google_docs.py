a1098bd1f991312de86348cd8e65c0782e8e4651
mayabrandi <maya.brandi@scilifelab.se>
2014-12-10 14:27:37 +0100

    doc

--- LIMS2DB/objectsDB/load_status_from_google_docs.py
+++ LIMS2DB/objectsDB/load_status_from_google_docs.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
-import sys
-import os
-import time
-from  datetime  import  datetime
-from uuid import uuid4
-import hashlib
-from optparse import OptionParser
-import logging
-import bcbio.pipeline.config_utils as cl
-from scilifelab.google.google_docs import SpreadSheet
-from scilifelab.google import get_credentials
-import couchdb
+#import sys
+#import os
+#import time
+#from  datetime  import  datetime
+#from uuid import uuid4
+#import hashlib
+#from optparse import OptionParser
+#import logging
+#import bcbio.pipeline.config_utils as cl
+#from scilifelab.google.google_docs import SpreadSheet
+#from scilifelab.google import get_credentials
+#import couchdb
 
 
 #           GOOGLE DOCS
