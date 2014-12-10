286cde7db693db8aa7342b35a154f2215b7ac157
mayabrandi <maya.brandi@scilifelab.se>
2014-12-10 16:00:44 +0100

    s

--- LIMS2DB/objectsDB/objectsDB.py
+++ LIMS2DB/objectsDB/objectsDB.py
@@ -6,14 +6,14 @@ statusdb with lims as the main source of information.
 Maya Brandi, Science for Life Laboratory, Stockholm, Sweden.
 """
 import codecs
-#from genologics.lims import *
-#import genologics.entities as gent
-#from genologics.lims_utils import *
+from genologics.lims import *
+import genologics.entities as gent
+from genologics.lims_utils import *
 from process_categories import *
-#from statusdb.db.utils import *
+from statusdb.db.utils import *
 from functions import *
 import os
-#import couchdb
+import couchdb
 import time
 from datetime import date
 import logging
